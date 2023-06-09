239296d9507227decc68539569cfa64db087b8e9
Marcus Warm <warm@mwvb.de>
2023-06-09 21:50:17 +0200

    move page: book symbol for book entry

--- minerva/src/main/java/minerva/seite/MoveSeitePage.java
+++ minerva/src/main/java/minerva/seite/MoveSeitePage.java
@@ -5,7 +5,6 @@ import minerva.model.SeiteSO;
 import minerva.model.SeitenSO;
 
 public class MoveSeitePage extends SPage {
-    // TODO beim Bucheintrag dahinter " (Buch)" schreiben (oder ein Buchsymbol davor machen)
     
     @Override
     protected void execute() {
@@ -13,14 +12,17 @@ public class MoveSeitePage extends SPage {
         put("pageTitle", esc(seite.getTitle()));
 
         StringBuilder gliederung = new StringBuilder();
-        gliederung.append("<ul><li><a href=\"/s/" + branch + "/" + bookFolder + "/" + esc(id)
-                + "/moved?parentid=root\">" + esc(book.getTitle()) + "</a></li>");
+        gliederung.append("<ul><li>"
+                + "<i class=\"fa fa-book\" style=\"color: #090;\"></i> "
+                + "<a href=\"/s/" + branch + "/" + bookFolder + "/" + esc(id) + "/moved?parentid=root\">"
+                + esc(book.getTitle()) + "</a></li>");
         fillSeiten(branch, bookFolder, book.getSeiten(), user.getLanguage(), gliederung, false);
         gliederung.append("</ul>");
         put("gliederung", gliederung.toString());
     }
 
-    private void fillSeiten(String branch, String bookFolder, SeitenSO seiten, String lang, StringBuilder gliederung, boolean noLink) {
+    private void fillSeiten(String branch, String bookFolder, SeitenSO seiten, String lang,
+            StringBuilder gliederung, boolean noLink) {
         // Wegen der Rekursion ist eine Template-Datei nicht sinnvoll.
         gliederung.append("<ul>\n");
         for (SeiteSO seite : seiten) {
@@ -28,7 +30,8 @@ public class MoveSeitePage extends SPage {
                 gliederung.append("\t<li>" + esc(seite.getSeite().getTitle().getString(lang)) + "</li>\n");
                 fillSeiten(branch, bookFolder, seite.getSeiten(), lang, gliederung, true);
             } else if (seite.getId().equals(id)) {
-                gliederung.append("\t<li style=\"font-weight: bold;\">" + esc(seite.getSeite().getTitle().getString(lang)) + "</li>\n");
+                gliederung.append("\t<li style=\"font-weight: bold;\">"
+                        + esc(seite.getSeite().getTitle().getString(lang)) + "</li>\n");
                 fillSeiten(branch, bookFolder, seite.getSeiten(), lang, gliederung, true);
             } else if (this.seite.getSeite().getParentId().equals(seite.getId())) {
                 // current parent page
