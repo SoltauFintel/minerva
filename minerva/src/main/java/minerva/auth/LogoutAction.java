627629d692c5db1680f87f7b25a3dd332c61e514
Marcus Warm <warm@mwvb.de>
2023-06-19 18:07:07 +0200

    Logout bugfix

--- minerva/src/main/java/minerva/auth/LogoutAction.java
+++ minerva/src/main/java/minerva/auth/LogoutAction.java
@@ -5,13 +5,17 @@ import github.soltaufintel.amalia.auth.rememberme.NoOpRememberMe;
 import github.soltaufintel.amalia.auth.webcontext.WebContext;
 import github.soltaufintel.amalia.web.action.Action;
 import minerva.model.GitFactory;
+import minerva.model.StateSO;
 import minerva.model.StatesSO;
 
 public class LogoutAction extends Action {
 
     @Override
     protected void execute() {
-        GitFactory.logout(StatesSO.get(ctx).getUser().getUser());
+        StateSO stateSO = StatesSO.get(ctx);
+        if (stateSO != null) {
+            GitFactory.logout(stateSO.getUser().getUser());
+        }
         AuthService.logout(new WebContext(ctx), new NoOpRememberMe());
         ctx.req.session().invalidate();
         ctx.redirect("/");
