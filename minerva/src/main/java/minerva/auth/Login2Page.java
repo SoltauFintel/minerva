e9106b8dae12c0492b2da21b54cce9c7d52ebb90
Marcus Warm <warm@mwvb.de>
2023-06-21 16:31:33 +0200

    Old login page

--- minerva/src/main/java/minerva/auth/Login2Page.java
+++ minerva/src/main/java/minerva/auth/Login2Page.java
@@ -1,5 +1,15 @@
 package minerva.auth;
 
+import org.pmw.tinylog.Logger;
+
+/**
+ * Old login page with user and password
+ */
 public class Login2Page extends LoginPage {
 
+    @Override
+    protected void execute() {
+        Logger.info("old login page");
+        super.execute();
+    }
 }
