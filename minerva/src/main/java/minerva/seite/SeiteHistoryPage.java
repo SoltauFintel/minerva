7b8736069f908b65cae1ce8b52ded2d8350b48a0
Marcus Warm <warm@mwvb.de>
2023-06-21 09:22:52 +0200

    history

--- minerva/src/main/java/minerva/seite/SeiteHistoryPage.java
+++ minerva/src/main/java/minerva/seite/SeiteHistoryPage.java
@@ -1,12 +1,16 @@
 package minerva.seite;
 
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 import com.github.template72.data.DataList;
 import com.github.template72.data.DataMap;
 
 import minerva.MinervaWebapp;
 import minerva.git.HCommit;
+import minerva.model.GitlabRepositorySO;
 
 public class SeiteHistoryPage extends SPage {
 
@@ -16,7 +20,10 @@ public class SeiteHistoryPage extends SPage {
             throw new RuntimeException("Page only for Gitlab mode");
         }
 
-        List<HCommit> commits = MinervaWebapp.factory().getGitlabRepository().getSeiteMetaHistory(seite);
+        GitlabRepositorySO repo = MinervaWebapp.factory().getGitlabRepository();
+        String url = repo.getProjectUrl() + "/-/commit/"; // http://host:port/user/repo/-/commit/
+        List<HCommit> commits = repo.getSeiteMetaHistory(seite);
+        Set<String> authors = new TreeSet<>();
 
         header(seite.getTitle() + " - " + n("history"));
         DataList list = list("commits");
@@ -24,9 +31,14 @@ public class SeiteHistoryPage extends SPage {
             DataMap map = list.add();
             map.put("hash", esc(commit.getHash()));
             map.put("hash7", esc(commit.getHash7()));
+            map.put("gitlabCommitLink", url + commit.getHash());
             map.put("author", esc(commit.getAuthor()));
             map.put("date", commit.getCommitDateTime());
             map.put("message", esc(commit.getMessage()));
+            authors.add(commit.getAuthor());
         }
+        put("authors", esc(authors.stream().collect(Collectors.joining(", "))));
+        putInt("n", commits.size());
+        put("filename", esc(seite.gitFilenameMeta()));
     }
 }
