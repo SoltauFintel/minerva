6c88bcf4e13dcfadda8a1e94911e30882d5d0d94
Marcus Warm <warm@mwvb.de>
2023-06-30 19:31:39 +0200

    bugfix

--- minerva/src/main/java/minerva/subscription/SubscriptionService.java
+++ minerva/src/main/java/minerva/subscription/SubscriptionService.java
@@ -55,7 +55,7 @@ public class SubscriptionService {
         // get book
         WorkspaceSO workspace = state.getUser().getWorkspaces().master();
         int n = workspace.getBooks().size();
-        if (n == 1) {
+        if (n != 1) {
             throw new RuntimeException("1 book is expected but there are " + n + " book(s).");
         }
         
