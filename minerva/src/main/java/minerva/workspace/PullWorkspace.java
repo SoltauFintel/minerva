0dee4ab90b65c377348d77195fea71dd1a1b0b1c
Marcus Warm <warm@mwvb.de>
2023-07-12 11:22:51 +0200

    BooksPage: pull workspace fixed for customer version

--- minerva/src/main/java/minerva/workspace/PullWorkspace.java
+++ minerva/src/main/java/minerva/workspace/PullWorkspace.java
@@ -1,6 +1,5 @@
 package minerva.workspace;
 
-import minerva.MinervaWebapp;
 import minerva.base.StringService;
 import minerva.user.UAction;
 
@@ -8,10 +7,6 @@ public class PullWorkspace extends UAction {
 
     @Override
     protected void execute() {
-        if (!MinervaWebapp.factory().isGitlab()) {
-            throw new RuntimeException("Page only for Gitlab mode");
-        }
-
         String branch = ctx.pathParam("branch");
         boolean force = "1".equals(ctx.queryParam("force"));
         String book = ctx.queryParam("book");
