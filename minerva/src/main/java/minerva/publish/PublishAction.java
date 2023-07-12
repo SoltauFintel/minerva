4cde113b43a93af67aa6936f1d34f1277d324179
Marcus Warm <warm@mwvb.de>
2023-07-12 14:53:42 +0200

    PublishAction sollte nicht zeitgleich mehrfach aufgerufen werden!

--- minerva/src/main/java/minerva/publish/PublishAction.java
+++ minerva/src/main/java/minerva/publish/PublishAction.java
@@ -17,7 +17,7 @@ import spark.Response;
 import spark.Route;
 
 public class PublishAction implements Route {
-    // TODO PublishAction sollte nicht zeitgleich mehrfach aufgerufen werden!
+    private static String handle = "handle";
 
     @Override
     public Object handle(Request req, Response res) throws Exception {
@@ -30,23 +30,25 @@ public class PublishAction implements Route {
         for (String lang: langsStr.split(",")) {
             langs.add(lang);
         }
-        UserSO userSO = new UserSO(new GitlabUser(login, password));
-        WorkspaceSO workspace = userSO.getWorkspace(branch);
-        String publishFolder = System.getenv("MINERVA_PUBLISHFOLDER");
-        if (StringService.isNullOrEmpty(publishFolder)) {
-            throw new RuntimeException("Env var MINERVA_PUBLISHFOLDER is not set. Typical value is \"/tmp/publish\".");
+        synchronized (handle) {
+            UserSO userSO = new UserSO(new GitlabUser(login, password));
+            WorkspaceSO workspace = userSO.getWorkspace(branch);
+            String publishFolder = System.getenv("MINERVA_PUBLISHFOLDER");
+            if (StringService.isNullOrEmpty(publishFolder)) {
+                throw new RuntimeException("Env var MINERVA_PUBLISHFOLDER is not set. Typical value is \"/tmp/publish\".");
+            }
+            File targetFolder = new File(publishFolder);
+    
+            PublishService ps = new PublishService(targetFolder, langs);
+            ps.publish(workspace);
+            Path zipFile = ps.zip();
+    
+            res.type("application/zip");
+            res.header("Content-Disposition", "attachment; filename=\"publish.zip\"");
+            res.raw().getOutputStream().write(Files.readAllBytes(zipFile));
+    
+            Logger.info("PublishAction finished");
+            return res.raw();
         }
-        File targetFolder = new File(publishFolder);
-
-        PublishService ps = new PublishService(targetFolder, langs);
-        ps.publish(workspace);
-        Path zipFile = ps.zip();
-
-        res.type("application/zip");
-        res.header("Content-Disposition", "attachment; filename=\"publish.zip\"");
-        res.raw().getOutputStream().write(Files.readAllBytes(zipFile));
-
-        Logger.info("PublishAction finished");
-        return res.raw();
     }
 }
