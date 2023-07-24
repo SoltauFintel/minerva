5e1a6287140e0599a28e933d01651caaa909a671
Marcus Warm <marcus.warm@x-map.de>
2023-07-24 10:59:04 +0200

    Eclipse

--- minerva/src/main/java/minerva/MinervaWebapp.java
+++ minerva/src/main/java/minerva/MinervaWebapp.java
@@ -92,7 +92,7 @@ import minerva.workspace.WorkspaceHistoryPage;
 import minerva.workspace.WorkspacesPage;
 
 public class MinervaWebapp extends RouteDefinitions {
-    public static final String VERSION = "0.2.0";
+    public static final String VERSION = "0.3.0";
     private static MinervaFactory factory;
     
     @Override
