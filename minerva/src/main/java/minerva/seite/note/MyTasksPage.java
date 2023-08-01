e5e8dbc0b452fd839047b18c5e8b6cfa3a6f3245
Marcus Warm <marcus.warm@x-map.de>
2023-08-01 09:40:35 +0200

    real name

--- minerva/src/main/java/minerva/seite/note/MyTasksPage.java
+++ minerva/src/main/java/minerva/seite/note/MyTasksPage.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.pmw.tinylog.Logger;
 
+import minerva.MinervaWebapp;
 import minerva.base.StringService;
 import minerva.base.Uptodatecheck;
 import minerva.workspace.WPage;
@@ -14,7 +15,7 @@ public class MyTasksPage extends WPage implements Uptodatecheck {
     protected void execute() {
     	String login = ctx.queryParam("login");
 		Logger.info(user.getLogin() + " | " + (user.getLogin().equals(login) ? "My tasks" : "All tasks for " + login));
-		put("login", esc(StringService.isNullOrEmpty(login) ? user.getLogin() : login));
+		put("login", esc(MinervaWebapp.factory().login2RealName(StringService.isNullOrEmpty(login) ? user.getLogin() : login)));
         header(n("myTasks"));
         List<NoteWithSeite> notes = user.getNotes(branch, login);
 		AllNotesPage.fill(notes, branch, model, user.getLogin());
