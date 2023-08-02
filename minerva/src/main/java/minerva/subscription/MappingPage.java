253daa509430ac28b07029f17374d9558f0112ca
Marcus Warm <warm@mwvb.de>
2023-08-02 12:13:55 +0200

    refactoring

--- minerva/src/main/java/minerva/subscription/MappingPage.java
+++ minerva/src/main/java/minerva/subscription/MappingPage.java
@@ -15,40 +15,47 @@ public class MappingPage extends SPage {
         PageTitles titles = new SubscriptionService().loadPageTitles();
         
         header(n("customerMapping"));
-        DataList list = list("titles");
-        DataList list2 = list("linked");
-        boolean hasMappings = false;
+        put("delete", n("delete").toLowerCase());
+        DataList titles_list = list("titles");
+        DataList linked_list = list("linked");
+
         if (titles != null && titles.getLang() != null) {
             List<PageTitle> titleList = titles.getLang().get(user.getPageLanguage());
             titleList.sort((a, b) -> StringService.umlaute(a.getTitle()).compareTo(
-                                     StringService.umlaute(b.getTitle())));
-            for (PageTitle t : titleList) {
-                DataMap map = list.add();
-                String url = viewlink + "/add-mapping?ohid=" + u(t.getId());
-                map.put("link", esc(url));
-                map.put("forcelink", esc(url + "&m=f"));
-                map.put("title", esc(t.getTitle()));
-            }
-            String forceText = n("force");
-            for (String key : seite.getSeite().getHelpKeys()) {
-                boolean force = key.endsWith("!");
-                if (force) {
-                    key = key.substring(0, key.length() - "!".length());
-                }
-                DataMap map2 = list2.add();
-                String x = "#" + key;
-                for (PageTitle t : titleList) {
-                    if (t.getId().equals(key)) {
-                        x = t.getTitle();
-                        break;
-                    }
-                }
-                map2.put("title", esc(x + (force ? " (" + forceText + ")" : "")));
-                map2.put("link", esc(viewlink + "/add-mapping?m=d&ohid=" + u(key)));
-                hasMappings = true;
-            }
+                    StringService.umlaute(b.getTitle())));
+            createPageTitlesList(titleList, titles_list);
+            createLinkedPagesList(titleList, linked_list);
+        } else {
+            put("hasMappings", false);
         }
-        put("delete", n("delete").toLowerCase());
-        put("hasMappings", hasMappings);
+    }
+
+    private void createPageTitlesList(List<PageTitle> titleList, DataList list) {
+        for (PageTitle title : titleList) {
+            DataMap map = list.add();
+            String url = viewlink + "/add-mapping?ohid=" + u(title.getId());
+            map.put("link", esc(url));
+            map.put("forcelink", esc(url + "&m=f"));
+            map.put("title", esc(title.getTitle()));
+        }
+    }
+
+    private void createLinkedPagesList(List<PageTitle> titleList, DataList list) {
+        String forceText = n("force");
+        for (String key : seite.getSeite().getHelpKeys()) {
+            boolean force = key.endsWith("!");
+            if (force) {
+                key = key.substring(0, key.length() - "!".length());
+            }
+            String theTitle = getTitle(key, titleList) + (force ? " (" + forceText + ")" : "");
+            DataMap map = list.add();
+            map.put("title", esc(theTitle));
+            map.put("link", esc(viewlink + "/add-mapping?m=d&ohid=" + u(key)));
+            put("hasMappings", true);
+        }
+    }
+
+    private String getTitle(String key, List<PageTitle> titleList) {
+        return titleList.stream().filter(title -> title.getId().equals(key)).map(title -> title.getTitle()).findFirst().orElse("#" + key);
     }
 }
