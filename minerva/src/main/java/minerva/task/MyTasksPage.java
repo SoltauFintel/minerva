642e7e53bb4dde0397fad72120f72e6c31823d2e
Marcus Warm <marcus.warm@x-map.de>
2023-11-17 13:57:57 +0100

    MyTasksPage verallgemeinert

--- minerva/src/main/java/minerva/task/MyTasksPage.java
+++ minerva/src/main/java/minerva/task/MyTasksPage.java
@@ -1,14 +1,12 @@
 package minerva.task;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.pmw.tinylog.Logger;
 
 import com.github.template72.data.DataList;
 import com.github.template72.data.DataMap;
 
-import github.soltaufintel.amalia.web.action.Escaper;
 import minerva.base.StringService;
 import minerva.base.Uptodatecheck;
 import minerva.seite.Note;
@@ -29,41 +27,32 @@ public class MyTasksPage extends WPage implements Uptodatecheck {
 		putInt("n", notes.size());
     }
     
-    private static void fill(List<NoteWithSeite> notes, String branch, DataMap model, String login) {
+    private void fill(List<NoteWithSeite> notes, String branch, DataMap model, String login) {
         String v0 = "/s/" + branch + "/";
         notes.sort((a, b) -> b.getNote().getCreated().compareTo(a.getNote().getCreated()));
         DataList list = model.list("notes");
         for (NoteWithSeite n : notes) {
             DataMap map = list.add();
             Note note = n.getNote();
-            map.put("noteId", note.getId());
             map.put("id", n.getSeite().getId() + "-" + note.getId());
             map.put("me", note.getUser().equals(login));
-            map.put("user", mesc(UserAccess.login2RealName(note.getUser())));
-            map.put("created", mesc(note.getCreated()));
+            map.put("user", esc(UserAccess.login2RealName(note.getUser())));
+            map.put("date", esc(note.getCreated()));
             String text = note.getText();
             if (text.length() > 113) {
-                map.put("text1", StringService.makeClickableLinks(mesc(text.substring(0, 110))));
+                map.put("text1", StringService.makeClickableLinks(esc(text.substring(0, 110))));
                 map.put("hasMoreText", true);
             } else {
-                map.put("text1", StringService.makeClickableLinks(mesc(text)));
+                map.put("text1", StringService.makeClickableLinks(esc(text)));
                 map.put("hasMoreText", false);
             }
-            map.put("completeText", StringService.makeClickableLinks(mesc(text)));
+            map.put("completeText", StringService.makeClickableLinks(esc(text)));
             String bookFolder = n.getSeite().getBook().getBook().getFolder();
             String v = v0 + bookFolder + "/" + n.getSeite().getId();
+            map.put("link", v + "/notes?highlight=" + note.getId() + "#" + note.getId());
             map.put("pagelink", v);
-            map.put("link", v + "/notes");
-            map.put("linkh", v + "/notes?highlight=" + note.getId() + "#" + note.getId());
-            map.put("bookTitle", mesc(n.getSeite().getBook().getTitle()));
-            map.put("pageTitle", mesc(n.getSeite().getTitle()));
-            map.put("hasPersons", !note.getPersons().isEmpty());
-            map.put("persons", mesc(note.getPersons().stream().collect(Collectors.joining(", "))));
+            map.put("pageTitle", esc(n.getSeite().getTitle()));
         }
         model.putInt("n", notes.size());
-    }
-    
-    private static String mesc(String text) {
-        return Escaper.esc(text);
     }
 }
