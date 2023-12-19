43e2b5faa64294e9ee6365181874e53603407e47
Marcus Warm <warm@mwvb.de>
2023-12-19 16:49:15 +0100

    note

--- minerva/src/main/java/minerva/MinervaWebapp.java
+++ minerva/src/main/java/minerva/MinervaWebapp.java
@@ -152,7 +152,7 @@ public class MinervaWebapp extends RouteDefinitions {
 
         // Export
         get("/w/:branch/books/export", ExportWorkspaceAction.class);
-        form("/w/:branch/export", ExportPage.class);
+        form("/w/:branch/export", ExportPage.class); // TODO mal testen, ob ich das <form action=...> Attribut weglassen kann
         form("/w/:branch/pages/export", SeitenauswahlPage.class);
         get("/w/:branch/download-export/:id/:dn", DownloadExportPage.class);
 
