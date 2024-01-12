738d225e905d681d047e2d17a7dfccd617e49cbb
Marcus Warm <warm@mwvb.de>
2024-01-12 18:43:37 +0100

    Successfully logged off

--- minerva/src/main/java/minerva/auth/LoggedOutPage.java
+++ minerva/src/main/java/minerva/auth/LoggedOutPage.java
@@ -3,22 +3,24 @@ package minerva.auth;
 import org.pmw.tinylog.Logger;
 
 import github.soltaufintel.amalia.web.action.Page;
+import minerva.MinervaWebapp;
 import minerva.base.NLS;
 
 public class LoggedOutPage extends Page {
-    private String lang;
+    private String lang = "en";
     
     @Override
     protected void execute() {
-        String lang = ctx.req.headers("Accept-Language");
-        Logger.debug("LoggedOutPage Accept-Language: " + lang);
-        if (lang.startsWith("de")) {
+        String al = ctx.req.headers("Accept-Language");
+        Logger.debug("LoggedOutPage Accept-Language: " + al);
+        if (al.startsWith("de")) {
             lang = "de";
         } else {
             lang = "en";
         }
         
         put("title", n("loggedOut"));
+        put("withRelogin", MinervaWebapp.factory().getConfig().isGitlab());
         put("relogin", n("relogin"));
         put("link", "/");
     }
