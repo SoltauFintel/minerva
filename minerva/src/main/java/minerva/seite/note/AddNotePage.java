38d85a9f0c112543276772635ab679c140a00850
Marcus Warm <warm@mwvb.de>
2024-02-05 16:05:06 +0100

    Revert "Notizenanalyse"
    This reverts commit cd85e1c4b2f6a93af0c405cc0c72354470fcc48f.

--- minerva/src/main/java/minerva/seite/note/AddNotePage.java
+++ minerva/src/main/java/minerva/seite/note/AddNotePage.java
@@ -3,15 +3,10 @@ package minerva.seite.note;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.google.common.base.Strings;
-
 import github.soltaufintel.amalia.spark.Context;
 import minerva.base.StringService;
-import minerva.model.BookSO;
-import minerva.model.SeiteSO;
 import minerva.seite.Note;
 import minerva.seite.SPage;
-import minerva.seite.note.Frequency2.FrequencyItem;
 import minerva.user.UserAccess;
 
 public class AddNotePage extends SPage {
@@ -41,23 +36,6 @@ public class AddNotePage extends SPage {
             put("parentId", parentId);
             combobox("persons", UserAccess.getUserNames(), selectedItems, true);
         }
-        
-        // Notizenanalyse XXX
-        Frequency2 f = new Frequency2();
-        for (BookSO bookSO : workspace.getBooks()) {
-            for (SeiteSO seiteSO : bookSO.getAlleSeiten()) {
-                for (Note note : seiteSO.getSeite().getNotes()) {
-                    if (note.getPersons() != null) {
-                        f.add("" + note.getPersons().size(), note.getId());
-                    }
-                }
-            }
-        }
-        String info = "";
-        for (FrequencyItem i : f.getList()) {
-            info += Strings.padStart("" + i.getMembers().size(), 6, ' ') + "x " + i.toString() + "<br/>";
-        }
-        put("info", info);
     }
 
     public static List<String> toPersons(Context ctx) {
