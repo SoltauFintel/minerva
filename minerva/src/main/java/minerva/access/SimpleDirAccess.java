967eb0c134e42821a0d59236a3ca37ff04671579
Marcus Warm <warm@mwvb.de>
2024-02-08 10:40:30 +0100

    bugfix: push comment images, delete images if comment is deleted

--- minerva/src/main/java/minerva/access/SimpleDirAccess.java
+++ minerva/src/main/java/minerva/access/SimpleDirAccess.java
@@ -26,13 +26,14 @@ public class SimpleDirAccess {
     public <T> void save(String id, T data, Set<String> images, CommitMessage commitMessage, String dir) {
         Map<String, String> files = new HashMap<>();
         files.put(dir + "/" + id + ".json", StringService.prettyJSON(data));
-        images.forEach(dn -> files.put(dir + "/img/" + id + "/" + dn, DirAccess.IMAGE));
+        images.forEach(dn -> files.put(dir + "/" + dn, DirAccess.IMAGE));
         dao.saveFiles(files, commitMessage, workspace);
     }
 
     public void delete(String id, CommitMessage commitMessage, String dir) {
         Set<String> filenames = new HashSet<>();
         filenames.add(dir + "/" + id + ".json");
+        filenames.add(dir + "/img/" + id + "/*");
         List<String> cantBeDeleted = new ArrayList<>();
         dao.deleteFiles(filenames, commitMessage, workspace, cantBeDeleted);
         if (!cantBeDeleted.isEmpty()) {
