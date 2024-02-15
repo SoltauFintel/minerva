541e96f4bd3495faf7485cb398a5d6a7a8385b4e
Marcus Warm <marcus.warm@x-map.de>
2024-02-15 16:25:56 +0100

    tag cloud

--- minerva/src/main/java/minerva/seite/tag/TagCloudPage.java
+++ minerva/src/main/java/minerva/seite/tag/TagCloudPage.java
@@ -5,36 +5,39 @@ import java.util.List;
 import com.github.template72.data.DataList;
 import com.github.template72.data.DataMap;
 
-import minerva.user.UPage;
+import minerva.workspace.WPage;
 
-public class TagCloudPage extends UPage {
+public class TagCloudPage extends WPage {
 
     @Override
     protected void execute() {
-        String branch = ctx.pathParam("branch");
-        boolean sortedByN = "n".equals(ctx.queryParam("m"));
+        List<TagN> tags = workspace.getAllTags().sortedByTag();
 
-        TagNList allTags = user.getWorkspace(branch).getAllTags();
-        List<TagN> tags = sortedByN ? allTags.sortedByN() : allTags.sortedByTag();
-
+        header(n("tagCloud"));
         DataList list = list("tags");
         for (TagN tag : tags) {
-            DataMap map2 = list.add();
-            map2.put("tag", esc(tag.getTag()));
-            map2.putInt("n", tag.getAnzahl());
-            map2.put("link", "/w/" + branch + "/tag/" + esc(tag.getTag()));
-            int size;
-            if (tag.getAnzahl() >= 30) {
-            	size = 28;
-            } else if (tag.getAnzahl() >= 10) {
-            	size = 22;
-            } else if (tag.getAnzahl() >= 3) {
-            	size = 16;
-            } else {
-            	size = 12;
-        	}
-            map2.putInt("size", size);
+            DataMap map = list.add();
+            map.put("tag", esc(tag.getTag()));
+            map.putInt("n", tag.getAnzahl());
+            map.put("link", "/w/" + branch + "/tag/" + esc(tag.getTag()));
+            map.putInt("size", size(tag));
         }
-        header(n("tagCloud"));
     }
+
+	private int size(TagN tag) {
+		int size;
+		final int n = tag.getAnzahl();
+		if (n >= 70) {
+			size = 35;
+		} else if (n >= 30) {
+			size = 27;
+		} else if (n >= 10) {
+			size = 22;
+		} else if (n >= 3) {
+			size = 16;
+		} else {
+			size = 11;
+		}
+		return size;
+	}
 }
