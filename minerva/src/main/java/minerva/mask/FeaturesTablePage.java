4bcd41636f28b5bdc03d57574063a2eae48049ef
Marcus Warm <warm@mwvb.de>
2024-04-12 10:34:09 +0200

    Features table: sort

--- minerva/src/main/java/minerva/mask/FeaturesTablePage.java
+++ minerva/src/main/java/minerva/mask/FeaturesTablePage.java
@@ -1,5 +1,6 @@
 package minerva.mask;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.github.template72.data.DataList;
@@ -17,7 +18,7 @@ public class FeaturesTablePage extends SPage {
     @Override
     protected void execute() {
         if (isPOST()) {
-            filter();
+            filterValues();
         } else {
             header(seite.getTitle());
             menu();
@@ -40,6 +41,7 @@ public class FeaturesTablePage extends SPage {
     }
 
     private void searchFields() {
+        String options = "<option></option>";
         DataList list = list("searchFields");
         if (mad0 != null) {
             final String st = """
@@ -53,15 +55,21 @@ public class FeaturesTablePage extends SPage {
             for (int i = 0; i < n; i += 3) {
                 String html = "";
                 for (int j = 0; j <= 2; j++) {
-                    MaskField f = fields.get(i + j);
-                    String val = ctx.formParam(f.getId());
-                    html += st.replace("{label}", f.getLabel()) //
-                            .replace("{id}", f.getId()) //
-                            .replace("{value}", val == null ? "" : val);
+                    if (i + j < fields.size()) {
+                        MaskField f = fields.get(i + j);
+                        String val = ctx.formParam(f.getId());
+                        html += st.replace("{label}", f.getLabel()) //
+                                .replace("{id}", f.getId()) //
+                                .replace("{value}", val == null ? "" : val);
+                        
+                        String sel = f.getId().equals(ctx.formParam("sort")) ? " selected" : "";
+                        options += "<option value=\"" + f.getId() + "\"" + sel + ">" + esc(f.getLabel()) + "</option>";
+                    }
                 }
                 list.add().put("html", html);
             }
         }
+        put("options", options);
     }
 
     private void columns() {
@@ -76,49 +84,75 @@ public class FeaturesTablePage extends SPage {
     
     private void rows() {
         DataList list = list("features");
+        String url0 = "/s/" + branch + "/" + book.getBook().getFolder() + "/";
         int k = 0, n = 0;
-        for (SeiteSO ft : seite.getSeiten()) {
+        for (TableEntry te : getFilteredSortedFeatures()) {
             String columns = "";
-            MaskAndDataFields mad = new MaskAndDataFields(ft);
-            
-            boolean drin = true;
-            for (MaskField f : mad.getMaskFields()) {
-                String v = ctx.formParam(f.getId());
-                if (!StringService.isNullOrEmpty(v)
-                        && !mad.getDataFields().get(f.getId()).toLowerCase().contains(v.toLowerCase())) {
-                    drin = false;
-                    break;
-                }
-            }
-            if (!drin) {
-                continue;
-            }
-
             boolean first = true;
-            for (MaskField f : mad.getMaskFields()) {
-                String v = mad.getDataFields().get(f.getId());
+            for (MaskField field : te.mad.getMaskFields()) {
+                String value = te.mad.getDataFields().get(field.getId());
                 if (first) {
-                    if (v.isBlank()) {
-                        v = "("  + n("empty") + ")";
-                    }
-                    columns += "<small>" + f.getLabel() + ": </small><a href=\"/s/" + branch + "/" + book.getBook().getFolder() + "/"
-                            + ft.getId() + "\"><b>" + v + "</b></a>";
                     first = false;
-                } else if (!v.isBlank() && !"SQLJasperReportInformation".equals(v)) {
-                    columns += "<br/><small>" + f.getLabel() + ": </small><b>" + v+"</b>";
+                    if (value.isBlank()) {
+                        value = "("  + n("empty") + ")";
+                    }
+                    String url = url0 + te.feature.getId();
+                    columns += "<small>" + field.getLabel() + ": </small>"
+                            + "<a href=\"" + url + "\"><b>" + value + "</b></a>";
+                } else if (!value.isBlank() && !"SQLJasperReportInformation".equals(value)) {
+                    columns += "<br/><small>" + field.getLabel() + ": </small><b>" + value + "</b>";
                 }
             }
             DataMap row = list.add();
             row.put("columns", columns);
             row.put("eins", ++k % 4 == 1);
-            row.put("hasText", ft.hasContentR("de") != 0);
-            row.put("hasOpenComments", new SeiteCommentService2(ft).getCommentState(ft.getLogin()) > 0);
+            row.put("hasText", te.feature.hasContentR("de") != 0);
+            row.put("hasOpenComments", new SeiteCommentService2(te.feature).getCommentState(te.feature.getLogin()) > 0);
             n++;
         }
         putInt("n", n);
     }
 
-    private void filter() {
+    private List<TableEntry> getFilteredSortedFeatures() {
+        List<TableEntry> features = new ArrayList<>();
+        for (SeiteSO ft : seite.getSeiten()) {
+            TableEntry te = new TableEntry(ft);
+            if (te.doFilter()) {
+                features.add(te);
+            }
+        }
+        
+        String sort = ctx.formParam("sort");
+        if (!StringService.isNullOrEmpty(sort)) {
+            features.forEach(te -> te.sort = te.mad.getDataFields().get(sort));
+            features.sort((a, b) -> a.sort.compareToIgnoreCase(b.sort));
+        }
+        return features;
+    }
+    
+    private class TableEntry {
+        public final SeiteSO feature;
+        public final MaskAndDataFields mad;
+        public String sort;
+        
+        public TableEntry(SeiteSO feature) {
+            this.feature = feature;
+            mad = new MaskAndDataFields(feature);
+        }
+        
+        public boolean doFilter() {
+            for (MaskField field : mad.getMaskFields()) {
+                String value = ctx.formParam(field.getId());
+                if (!StringService.isNullOrEmpty(value)
+                        && !mad.getDataFields().get(field.getId()).toLowerCase().contains(value.toLowerCase())) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    private void filterValues() {
         String q = "";
         if (!seite.getSeiten().isEmpty()) {
             String p = "?";
@@ -128,6 +162,9 @@ public class FeaturesTablePage extends SPage {
                     q += p + f.getId() + "=" + u(v);
                     p = "&";
                 }
+            }
+            if (!StringService.isNullOrEmpty(ctx.formParam("sort"))) {
+                q += p + "sort=" + u(ctx.formParam("sort"));
             }
         }
         ctx.redirect("/f/" + branch + "/" + bookFolder + "/" + seite.getId() + q);
