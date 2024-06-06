1d9123e360d79d4f93d49ff28c9f02a727a3cb7c
Marcus Warm <marcus.warm@x-map.de>
2024-06-06 13:17:21 +0200

    -Xmx950m

--- minerva/src/main/java/minerva/MinervaWebapp.java
+++ minerva/src/main/java/minerva/MinervaWebapp.java
@@ -145,7 +145,7 @@ import minerva.workspace.WorkspacesPage;
 import spark.Spark;
 
 public class MinervaWebapp extends RouteDefinitions {
-    public static final String VERSION = "2.1.0";
+    public static final String VERSION = "2.2.0";
     private static MinervaFactory factory;
     
     @Override
