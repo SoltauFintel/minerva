d2c963eb89540feb939d894e8dca5538d3a97fa6
Marcus Warm <marcus.warm@x-map.de>
2024-07-04 16:44:48 +0200

    Help

--- minerva/src/main/resources/en.rb
+++ minerva/src/main/resources/en.rb
@@ -374,3 +374,5 @@ kvdeletekey=Delete key type
 sameTitles=Same titles
 erlaeuterungen=Explanations
 validationErlaeuterungen=The "Validation" section lists error messages from some page checks. These errors should be corrected. "Same titles" list identical page titles. Identical titles are (technically) permitted and often do not constitute an error. Sometimes, however, it can happen unintentionally that two or more pages have been created on one topic. "Unused graphics" cannot be corrected by the user and do not cause any further problems. It is Minerva's job to clean this up. "External links": as long as a link is accessible to the readership, everything is fine.
+Help=Help
+MinervaHelp=Minerva Help
