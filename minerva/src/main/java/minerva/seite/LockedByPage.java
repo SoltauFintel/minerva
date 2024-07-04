802312ebdc7fe3235f0d90be99c775b417340920
Marcus Warm <warm@mwvb.de>
2024-07-04 10:43:07 +0200

    Lock screen in 2 stages

--- minerva/src/main/java/minerva/seite/LockedByPage.java
+++ minerva/src/main/java/minerva/seite/LockedByPage.java
@@ -4,13 +4,13 @@ import minerva.model.UserSO.LoginAndEndTime;
 import minerva.user.UserAccess;
 
 /**
- * Page editing is softly locked. The user can override this.
+ * Page editing is softly locked. The user can override this in 2 stages.
  */
 public class LockedByPage extends SPage {
     
     @Override
     protected void execute() {
-        String editlink = "/s-edit/" + branch + "/" + bookFolder + "/" + id;
+        final String editlink = "/s-edit/" + branch + "/" + bookFolder + "/" + id;
         LoginAndEndTime lockedBy = user.hasEditingStarted(workspace.getBranch(), id);
         if (lockedBy == null) { // Is page locked softly?
             // User pressed F5 - and now it's not locked anymore. So go to edit page.
@@ -19,12 +19,32 @@ public class LockedByPage extends SPage {
             return;
         }
         
-        header(n("locked.header"));
-        String key = user.getLogin().equals(lockedBy.getLogin()) ? "locked.info.self" : "locked.info";
-        put("msg", esc(n(key).replace("$t", seite.getTitle()).replace("$u", UserAccess.login2RealName(lockedBy.getLogin()))));
-        put("msg2", esc(n("locked.time").replace("$o", lockedBy.getEndTime())));
-        put("editlink", editlink + "?m=cl");
-        put("edit", n("locked.edit"));
+        String username = UserAccess.login2RealName(lockedBy.getLogin());
+        String goOnLink = editlink + "?m=cl";
+        
+        if ("2".equals(ctx.queryParam("s"))) { // Stage 2
+            fill(n("locked.header") + " (2)",
+                    n("locked.info2").replace("$t", seite.getTitle()).replace("$u", username),
+                    "",
+                    goOnLink,
+                    n("locked.edit2"));
+        } else { // Stage 1
+            boolean itsMe = user.getLogin().equals(lockedBy.getLogin());
+            String x = itsMe ? ".self" : "";
+            fill(n("locked.header"),
+                    n("locked.info1" + x).replace("$t", seite.getTitle()).replace("$u", username),
+                    n("locked.smallinfo1" + x).replace("$o", lockedBy.getEndTime()),
+                    itsMe ? goOnLink : viewlink + "/locked?s=2",
+                    n("locked.edit1" + x));
+        }
+    }
+    
+    private void fill(String header, String info, String smallinfo, String editlink, String editBtnLabel) {
+        header(header);
+        put("info", esc(info));
+        put("smallinfo", esc(smallinfo));
+        put("editlink", editlink);
+        put("edit", editBtnLabel);
         put("cancel", n("locked.no-edit"));
     }
 }
