260eb360fceefb68bfb2450f58cc83a51c4d7f44
Marcus Warm <warm@mwvb.de>
2024-07-12 11:08:45 +0200

    PageMenu

--- minerva/src/main/java/minerva/seite/ViewSeitePage.java
+++ minerva/src/main/java/minerva/seite/ViewSeitePage.java
@@ -85,10 +85,14 @@ public class ViewSeitePage extends SPage implements Uptodatecheck {
         editorComponent();
         header(modifyHeader(seite.getTitle()));
         fillLinks(branch, bookFolder, id, seite, _seite, u.getPageLanguage());
-        menu(isFavorite, pageWatched, subpagesWatched,
+        getPageMenu().menu(model, seite, viewlink, isAdmin, isFavorite, pageWatched, subpagesWatched,
                 MinervaWebapp.factory().getConfig().isGitlab(), MinervaWebapp.factory().isCustomerVersion()); // möglichst spät aufrufen
         Logger.info(u.getLogin() + " | " + seite.getBook().getWorkspace().getBranch() + " | "
                 + seite.getTitle() + " | " + u.getPageLanguage());
+    }
+    
+    protected PageMenu getPageMenu() {
+        return new PageMenu(user.getGuiLanguage());
     }
 
     private void simpleVars(User u, Seite _seite) {
@@ -387,71 +391,6 @@ public class ViewSeitePage extends SPage implements Uptodatecheck {
         return "";
     }
     
-    protected DataList menu(boolean isFavorite, boolean pageWatched, boolean subpagesWatched, boolean gitlab, boolean isCustomerVersion) {
-        DataList i = model.list("menuitems");
-        String viewLink = model.get("viewlink").toString();
-        menuitem(i, viewLink + "/toggle-favorite",
-                "fav fa-star" + (isFavorite ? "" : "-o"),
-                n("favorite") + (isFavorite ? " <i class=\"fa fa-check greenbook\"></i>" : ""));
-        menuitem(i, viewLink + "/toggle-watch",
-                "fa-bell" + (pageWatched ? "" : "-o"),
-                n("watchPage") + (pageWatched ? " <i class=\"fa fa-check greenbook\"></i>" : ""));
-        menuitem(i, viewLink + "/toggle-watch?m=s",
-                "fa-sitemap",
-                n("watchSubpages") + (subpagesWatched ? " <i class=\"fa fa-check greenbook\"></i>" : ""));
-        menuitem(i, " data-toggle=\"modal\" data-target=\"#tocModal\"", "fa-list-ul", n("TOC"));
-        menuitem(i, "", "", "-");
-        
-        if (gitlab) {
-            menuitem(i, viewLink + "/history", "fa-clock-o", n("history"));
-        }
-        if (isAdmin) {
-            //  li  style="background-color: #ff9;"
-            menuitem(i, " data-toggle=\"modal\" data-target=\"#editorsnoteModal\"",
-                    "fa-thumb-tack",
-                    n("editorsNote"));
-            
-        }
-        if (isCustomerVersion) {
-            menuitem(i, viewLink + "/help-keys",
-                    "fa-question-circle",
-                    n("helpKeys") + " (" + model.get("helpKeysSize").toString() + ")");
-        }
-        menuitem(i, viewLink + "/links", "fa-link", n("linkAnalysis"));
-        additionalMenuItems(i);
-        menuitem(i, "", "", "-");
-        
-        if (isAdmin) {
-            menuitem(i, viewlink + "/html", "fa-code", n("editHTML"));
-        }
-        menuitem(i, "/w/" + esc(branch) + "/export?seite=" + u(seite.getId()), "fa-upload", n("exportPage"));
-        menuitem(i, model.get("duplicatelink").toString(), "fa-copy", n("duplicatePage"));
-        menuitem(i, model.get("movelink").toString(), "fa-arrow-circle-right", n("movePage"));
-        menuitem(i, model.get("deletelink").toString(), "fa-trash", n("deletePage") + "...");
-        return i;
-    }
-    
-    protected void additionalMenuItems(DataList i) { //
-    }
-
-    protected void menuitem(DataList menuitems, String link, String icon, String label) {
-        DataMap map = menuitems.add();
-        map.put("link", link);
-        map.put("icon", icon);
-        map.put("label", label);
-        map.put("line", "-".equals(label));
-        map.put("attrs", "");
-        map.put("liArgs", "");
-        if (link.startsWith(" ")) {
-            map.put("link", "#");
-            map.put("attrs", link);
-        } else if (link.contains("/delete")) {
-            map.put("attrs", " style=\"color: #900;\"");
-        } else if (link.contains("editorsnote")) {
-            map.put("liArgs", " style=\"background-color: #ff9;\"");
-        }
-    }
-
     private boolean foundInOtherBook() {
         for (BookSO book : workspace.getBooks()) {
             if (!book.getBook().getFolder().equals(bookFolder)) {
