9b657ee4e9afd79fde1355b09f993bbe8c9cbdbd
Marcus Warm <warm@mwvb.de>
2024-07-15 08:59:44 +0200

    version 3.1.0

--- minerva/src/main/java/minerva/MinervaWebapp.java
+++ minerva/src/main/java/minerva/MinervaWebapp.java
@@ -148,7 +148,7 @@ import minerva.workspace.WorkspacesPage;
 import spark.Spark;
 
 public class MinervaWebapp extends RouteDefinitions {
-    public static final String VERSION = "3.0.0";
+    public static final String VERSION = "3.1.0";
     private static MinervaFactory factory;
     
     @Override
