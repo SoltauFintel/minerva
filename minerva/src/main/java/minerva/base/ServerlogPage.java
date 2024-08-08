d5c1640d763d6caefe3a450f4ba682c9d4654057
Marcus Warm <warm@mwvb.de>
2024-08-08 08:17:08 +0200

    changed sorting of serverlog display

--- minerva/src/main/java/minerva/base/ServerlogPage.java
+++ minerva/src/main/java/minerva/base/ServerlogPage.java
@@ -12,20 +12,7 @@ public class ServerlogPage extends UPage {
         user.onlyAdmin();
         user.log("-- Server log called.");
         
-        String c = user.getServerlog();
-        if (c != null) {
-            String[] lines = c.split("\n");
-            StringBuilder sb = new StringBuilder();
-            for (int i = lines.length - 1; i >= 0; i--) {
-                String line = lines[i];
-                sb.append(line);
-                if (i > 0) {
-                    sb.append("\n");
-                }
-            }
-            c = sb.toString();
-        }
         header(n("serverlog"));
-        put("serverlog", esc(c));
+        put("serverlog", esc(user.getServerlog()));
     }
 }
