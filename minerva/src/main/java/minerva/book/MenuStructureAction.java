40ed4213aae84626dfc57629cb5706dbd0a6af2a
Marcus Warm <marcus.warm@x-map.de>
2024-08-28 11:40:20 +0200

    Import menu structure

--- minerva/src/main/java/minerva/book/MenuStructureAction.java
+++ minerva/src/main/java/minerva/book/MenuStructureAction.java
@@ -1,42 +1,113 @@
 package minerva.book;
 
 import java.io.File;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.pmw.tinylog.Logger;
 
+import com.google.gson.Gson;
+
+import minerva.access.CommitMessage;
 import minerva.base.FileService;
+import minerva.base.StringService;
+import minerva.mask.FeatureFields;
+import minerva.mask.FeatureFieldsService;
+import minerva.model.BookSO;
 import minerva.model.SeiteSO;
 import minerva.model.SeitenSO;
 
 // XXX temp.
 public class MenuStructureAction extends BAction {
+    private final Gson gson = new Gson();
+    private int nextFeatureNumber;
     
     @Override
     protected void execute() {
-        String mode = ctx.queryParam("m");
+//        String mode = ctx.queryParam("m");
         
-        if ("input".equals(mode)) {
+//        if ("input".equals(mode)) {
             Logger.info("Menu... input");
             doImport();
-        } else { // output
-            Logger.info("Menu... output");
-            
-            StringBuilder sb = new StringBuilder();
-            add(book.getSeiten(), sb, "");
-            
-            File out = new File("export/menu.txt");
-            FileService.savePlainTextFile(out, sb.toString());
-            Logger.info(out.getAbsolutePath());
-        }
+//        } else { // output
+//            Logger.info("Menu... output");
+//            
+//            StringBuilder sb = new StringBuilder();
+//            add(book.getSeiten(), sb, "");
+//            
+//            File out = new File("export/menu.txt");
+//            FileService.savePlainTextFile(out, sb.toString());
+//            Logger.info(out.getAbsolutePath());
+//        }
     }
     
     private void doImport() {
         File in = new File("export/menu.txt");
         String text = FileService.loadPlainTextFile(in);
 		List<Item> items = getItems(text);
+		
 		findPages(items, book.getSeiten());
+
+		BookSO target = book.getWorkspace().getBooks().byFolder("featuretree");
+		SeiteSO targetPage = target.seiteById("pmd3e3"); // F1
+		Map<String, String> files = new HashMap<>();
+		nextFeatureNumber = 1;
+		createFeatures(items, targetPage, true, files);
+		Logger.info("saving " + files.size() + " files...");
+		target.dao().saveFiles(files, new CommitMessage("Saved menu structure as features"), book.getWorkspace());
+		Logger.info("saved");
+	}
+
+    private void createFeatures(List<Item> items, SeiteSO parentPage, boolean topLevel, Map<String,String> files) {
+    	int c = 0;
+		for (Item item : items) {
+			if (topLevel) {
+				c += 100;
+				nextFeatureNumber = c;
+				Logger.info(item.getTitle() + " = FMP" + new DecimalFormat("0000").format(nextFeatureNumber));
+			}
+			if (item.getFeatures() != null && item.getItems().isEmpty()) {
+				// many features for one page
+				for (String feature : item.getFeatures().split(",")) {
+					if (!feature.isBlank()) {
+						createFeature(item, feature, parentPage, files);
+					}
+				}
+			} else {
+				if (item.getFeatures() != null) {
+					Logger.warn("don't split features: " + item.getFeatures());
+				}
+				SeiteSO seite = createFeature(item, item.getTitle(), parentPage, files);
+				createFeatures(item.getItems(), seite, false, files); // recursive
+			}
+		}
+    }
+    
+	private SeiteSO createFeature(Item item, String title, SeiteSO parentPage, Map<String, String> files) {
+		String seiteId = parentPage.getSeiten().createSeite(parentPage, parentPage.getBook(), parentPage.getBook().dao());
+		SeiteSO seite = parentPage.getSeiten().byId(seiteId);
+
+		seite.getSeite().getTitle().setString("de", title);
+		seite.getSeite().getTitle().setString("en", title);
+
+		seite.saveMetaTo(files);
+
+		boolean hasFeatureNumber = item.getItems().isEmpty();
+		boolean hasHandbuchLink = !StringService.isNullOrEmpty(item.getSeiteId());
+		if (hasFeatureNumber || hasHandbuchLink) {
+			FeatureFields ff = FeatureFields.create(seite);
+			if (hasFeatureNumber) {
+				ff.set("featurenumber", "FMP" + new DecimalFormat("0000").format(nextFeatureNumber++));
+			}
+			if (hasHandbuchLink) {
+				ff.getPages().add(item.getSeiteId()); // link to handbuch page
+			}
+			files.put(FeatureFieldsService.dn(seite), gson.toJson(ff));
+		}
+		return seite;
 	}
 
 	private void findPages(List<Item> items, SeitenSO seiten) {
