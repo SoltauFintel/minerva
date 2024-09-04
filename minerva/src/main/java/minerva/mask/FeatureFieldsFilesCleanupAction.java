81a0fa7684d15cbe53cac7941d2f3146e0d7b8fd
Marcus Warm <marcus.warm@x-map.de>
2024-09-04 12:38:26 +0200

    Feature fields files cleanup

--- minerva/src/main/java/minerva/mask/FeatureFieldsFilesCleanupAction.java
+++ minerva/src/main/java/minerva/mask/FeatureFieldsFilesCleanupAction.java
@@ -1,10 +1,13 @@
 package minerva.mask;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.pmw.tinylog.Logger;
 
+import minerva.access.CommitMessage;
 import minerva.book.BAction;
 import minerva.model.SeiteSO;
 
@@ -16,25 +19,35 @@ public class FeatureFieldsFilesCleanupAction extends BAction {
 		if (!book.isFeatureTree()) {
 			throw new RuntimeException("Only for feature tree");
 		}
-		
-		Set<String> filenames = book.dao().getFilenames(book.getFolder() + "/feature-fields");
+
+		final String bf = book.getFolder();
+		Set<String> filenames = book.dao().getFilenames(bf + "/feature-fields");
 		Set<String> existing = new HashSet<>();
+		Set<String> kill = new HashSet<>();
 		for (SeiteSO ft : book.getAlleSeiten()) {
 			existing.add(ft.getId() + ".ff");
 		}
-		Logger.info(book.getFolder() + " | Diese .ff Dateien sind Leichen:");
-		int n = 0, ex = 0;
+		Logger.info(bf + " | .ff Dateileichen löschen");
+		int ok = 0;
 		for (String dn : filenames) {
-			if (!existing.contains(dn)) {
-				Logger.info(dn);
-				n++;
+			if (existing.contains(dn)) {
+				ok++;
 			} else {
-				ex++;
+				kill.add(bf + "/" + dn);
 			}
 		}
-		Logger.info("Anzahl: " + n + " | ok: " + ex);
-		
-		// TODO Wenn Features neu importiert werden bzw. gelöscht werden, muss auch die .ff Datei gelöscht werden.
+		Logger.info("Anzahl: " + kill.size() + " | ok: " + ok);
+		if (!kill.isEmpty()) {
+			List<String> cant = new ArrayList<>();
+			book.dao().deleteFiles(kill, new CommitMessage("Delete unused *.ff files"), workspace, cant);
+			if (cant.isEmpty()) {
+				Logger.info("Löschen erfolgreich");
+			} else {
+				Logger.error("Konnte nicht gelöscht werden: " + cant.toString());
+			}
+		}
+
+		// TODO Wenn Features neu importiert werden, müssen auch die alten .ff Dateien gelöscht werden.
 
 		ctx.redirect("/");
 	}
