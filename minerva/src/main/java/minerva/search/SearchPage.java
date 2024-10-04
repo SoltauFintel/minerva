1976932fb801cecad2593f9b88c271d49a3bf3c7
Marcus Warm <marcus.warm@x-map.de>
2024-10-04 18:47:04 +0200

    Refactoring

--- minerva/src/main/java/minerva/search/SearchPage.java
+++ minerva/src/main/java/minerva/search/SearchPage.java
@@ -13,7 +13,8 @@ import minerva.base.StringService;
 import minerva.user.UPage;
 
 public class SearchPage extends UPage {
-
+	private int n;
+	
     @Override
     protected void execute() {
         String branch = ctx.pathParam("branch");
@@ -22,16 +23,8 @@ public class SearchPage extends UPage {
         if (isPOST()) {
             ctx.redirect("/w/" + esc(branch) + "/search?q=" + u(q));
         } else {
-            Map<String, List<SearchResult>> results = new HashMap<>();
-            int n = 0;
-            for (String lang : langs) { 
-                List<SearchResult> result = user.getWorkspace(branch).getSearch().search(q, lang);
-                int nn = result.size();
-                if (nn > 0) {
-                    n += nn;
-                    results.put(lang, result);
-                }
-            }
+        	n = 0;
+            Map<String, List<SearchResult>> results = getResults(branch, q);
             Logger.info(user.getLogin() + " | " + branch
                     + " | Search for \"" + q + "\": " + n + " page" + (n == 1 ? "" : "s"));
 
@@ -39,21 +32,38 @@ public class SearchPage extends UPage {
             put("searchFocus", true);
             put("q", esc(q));
             put("hasq", !StringService.isNullOrEmpty(q));
-            DataList list = list("langs");
-            results.entrySet().stream()
-                .sorted((a, b) -> Integer.compare(b.getValue().size(), a.getValue().size()))
-                .forEach(e -> {
-                    DataMap map = list.add();
-                    map.put("lang", esc(e.getKey()));
-                    DataList list2 = map.list("result");
-                    for (SearchResult s : e.getValue()) {
-                        DataMap map2 = list2.add();
-                        map2.put("title", esc(s.getTitle()));
-                        map2.put("path", esc(s.getPath()));
-                        map2.put("content", s.getContent());
-                    }
-                });
+            fillList(results);
             putInt("n", n);
         }
     }
+
+	private Map<String, List<SearchResult>> getResults(String branch, String q) {
+		Map<String, List<SearchResult>> results = new HashMap<>();
+		for (String lang : langs) { 
+		    List<SearchResult> result = user.getWorkspace(branch).getSearch().search(q, lang);
+		    int nn = result.size();
+		    if (nn > 0) {
+		        n += nn;
+		        results.put(lang, result);
+		    }
+		}
+		return results;
+	}
+
+	private void fillList(Map<String, List<SearchResult>> results) {
+		DataList list = list("langs");
+		results.entrySet().stream()
+		    .sorted((a, b) -> Integer.compare(b.getValue().size(), a.getValue().size()))
+		    .forEach(e -> {
+		        DataMap map = list.add();
+		        map.put("lang", esc(e.getKey()));
+		        DataList list2 = map.list("result");
+		        for (SearchResult s : e.getValue()) {
+		            DataMap map2 = list2.add();
+		            map2.put("title", esc(s.getTitle()));
+		            map2.put("path", esc(s.getPath()));
+		            map2.put("content", s.getContent());
+		        }
+		    });
+	}
 }
