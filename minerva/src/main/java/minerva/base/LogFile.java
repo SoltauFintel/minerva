ef07812e9e5f6852a1087f2e7df2cccccb23ac15
Marcus Warm <marcus.warm@x-map.de>
2024-11-20 09:11:59 +0100

    LogFile

---
+++ minerva/src/main/java/minerva/base/LogFile.java
@@ -0,0 +1,28 @@
+package minerva.base;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.pmw.tinylog.Logger;
+
+import minerva.MinervaWebapp;
+
+public class LogFile {
+	private final File file;
+	
+	public LogFile(String dn) {
+		file = new File(MinervaWebapp.factory().getConfig().getWorkspacesFolder(), dn);
+		file.getParentFile().mkdirs();
+	}
+	
+	public void write(String line) {
+		try (FileWriter w = new FileWriter(file, true)) {
+			w.write(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss ")) + line + "\r\n");
+		} catch (IOException e) {
+			Logger.warn(e.getMessage() + " -> " + line);
+		}
+	}
+}
