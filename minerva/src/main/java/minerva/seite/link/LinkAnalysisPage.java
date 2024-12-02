b6053e893aae4f41d0c2753609394aa7d19531e0
Marcus Warm <marcus.warm@x-map.de>
2024-12-02 14:25:07 +0100

    LinkAnalysisPage TableComponent

--- minerva/src/main/java/minerva/seite/link/LinkAnalysisPage.java
+++ minerva/src/main/java/minerva/seite/link/LinkAnalysisPage.java
@@ -5,6 +5,8 @@ import java.util.List;
 import com.github.template72.data.DataList;
 import com.github.template72.data.DataMap;
 
+import minerva.base.TableComponent;
+import minerva.base.TableComponent.Col;
 import minerva.book.BookPage;
 import minerva.seite.SPage;
 
@@ -25,7 +27,23 @@ public class LinkAnalysisPage extends SPage {
             map.put("internal", e.isInternal());
             map.put("outgoing", e.isOutgoing());
             map.put("crossbook", e.isCrossBook());
+            
+            map.put("c2", (e.isOutgoing() ? "o" : "i") + (e.isCrossBook() ? "c" : (e.isInternal() ? "i" : "e")));
+            map.put("c3", e.isCrossBook() ? "" : e.getLinkTitle());
+            map.put("c4", e.getPageTitle());
         }
+        List<Col> cols = List.of(
+        		new Col(n("language"), "{{i.lang}}").sortable("lang"),
+        		new Col(n("type"),
+        		"""
+		        {{if i.outgoing}}<span style="color: #0a0;">outgoing</span>{{else}}<span style="color: blue;">incoming</span>{{/if}}
+		        {{if i.crossbook}}<span style="color: #909;">cross-book</span>{{else if i.internal}}internal{{else}}<span style="color: #c00;">external</span>{{/if}}
+        		""").sortable("c2"),
+        		new Col(n("linkTitle"), "{{if not i.crossbook}}<a href=\"{{i.href}}\">{{i.linkTitle}}</a>{{/if}}")
+        			.sortable("c3"),
+        		new Col(n("pageTitle"), "{{if i.crossbook}}<a href=\"{{i.href}}\">{{i.pageTitle}}</a>{{else}}{{i.pageTitle}}{{/if}}")
+        			.sortable("c4"));
+        put("table1", new TableComponent("wauto", cols, model, "links"));
         put("hasLinks", !result.isEmpty());
 
         if (book.isNotPublic()) {
