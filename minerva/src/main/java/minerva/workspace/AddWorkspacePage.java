ee5da9983351bc66e86a924fd044c76000c163a3
Marcus Warm <warm@mwvb.de>
2024-12-16 14:42:15 +0100

    Create workspace: select branch and first book after creation

--- minerva/src/main/java/minerva/workspace/AddWorkspacePage.java
+++ minerva/src/main/java/minerva/workspace/AddWorkspacePage.java
@@ -3,6 +3,7 @@ package minerva.workspace;
 import java.util.List;
 
 import minerva.MinervaWebapp;
+import minerva.model.WorkspaceSO;
 import minerva.user.UPage;
 
 public class AddWorkspacePage extends UPage {
@@ -11,8 +12,15 @@ public class AddWorkspacePage extends UPage {
     protected void execute() {
         MinervaWebapp.factory().gitlabOnlyPage();
         if (isPOST()) {
-            user.getWorkspaces().addWorkspace(ctx.formParam("branch"), user);
-            ctx.redirect("/");
+            String branch = ctx.formParam("branch");
+            WorkspaceSO newWorkspace = user.getWorkspaces().addWorkspace(branch, user);
+            String path;
+            try {
+                path = "/b/" + esc(newWorkspace.getBranch()) + "/" + esc(newWorkspace.getBooks().get(0).getBook().getFolder());
+            } catch (Exception e) {
+                path = "/";
+            }
+            ctx.redirect(path);
         } else {
             List<String> branchs = user.getWorkspaces().getAddableBranches(user.masterWorkspace());
             if (branchs.isEmpty()) {
