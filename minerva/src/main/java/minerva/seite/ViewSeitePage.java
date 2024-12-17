153bba86fd868309fdc0bcbdbee3988002e56402
Marcus Warm <marcus.warm@x-map.de>
2024-12-17 09:37:03 +0100

    CSSFilter

--- minerva/src/main/java/minerva/seite/ViewSeitePage.java
+++ minerva/src/main/java/minerva/seite/ViewSeitePage.java
@@ -12,6 +12,7 @@ import com.github.template72.data.DataMap;
 import github.soltaufintel.amalia.web.action.Escaper;
 import gitper.base.StringService;
 import minerva.MinervaWebapp;
+import minerva.base.CSSFilter;
 import minerva.base.DeliverHtmlContent;
 import minerva.base.Uptodatecheck;
 import minerva.book.BookPage;
@@ -228,6 +229,9 @@ public class ViewSeitePage extends SPage implements Uptodatecheck {
         html = new LocalAnchors().transform(html); // after TocMacro.transform()
         html = Thumbnails.thumbnails(html, seite.getBook().getFolder(), seite.getId(), booklink.replace("/b/", "/s/") + "/");
         map.put("toc", macro.getTOC()); // no esc, after TocMacro.transform()
+        if ("en".equals(user.getGuiLanguage())) { // XXX temp.
+        	html = new CSSFilter().filter(html);
+        }
         return html;
     }
     
