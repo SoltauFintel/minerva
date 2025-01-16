4e2ad09a9bda095de3cf7a46477e2c8fca4b56a4
Marcus Warm <marcus.warm@x-map.de>
2025-01-16 15:42:23 +0100

    ViewSeitePage: includes and .js
    - Document.execCommand

--- minerva/src/main/resources/web/js/copy.js
+++ minerva/src/main/resources/web/js/copy.js
@@ -2,7 +2,7 @@ function copy(copyBtn, text) {
     let temp = $('<input>')
     $('body').append(temp)
     temp.val(text).select()
-    document.execCommand('copy')
+    Document.execCommand('copy')
     temp.remove()
     copyBtn.style.color = '#0a0'
     copyBtn.style.transform = 'scale(1.2) rotate(10deg)'
---
+++ minerva/src/main/resources/web/js/vspScripts.js
@@ -0,0 +1,18 @@
+document.querySelectorAll('.copy').forEach(copyBtn => {
+    copyBtn.addEventListener('click', () => {
+        const targetElement = document.querySelector(copyBtn.dataset.copy)
+        const text = targetElement.textContent.replace(/\s+/g, ' ')
+        let temp = $('<input>')
+        $('body').append(temp)
+        temp.val(text).select()
+        Document.execCommand('copy')
+        temp.remove()
+        copyBtn.style.color = '#0a0'
+        copyBtn.style.transform = 'scale(1.2) rotate(10deg)'
+        setTimeout(() => { copyBtn.style.color = ''; copyBtn.style.transform = ''; }, 500)
+    })
+})
+
+$('#tocModal').on('shown.bs.modal', function() {
+    $('#tocLevels').focus();
+})
