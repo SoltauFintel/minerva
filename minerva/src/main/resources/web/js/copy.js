456e6e13bb1a7d2dd1d96e3bf7ef31e58b508702
Marcus Warm <marcus.warm@x-map.de>
2025-01-16 16:50:46 +0100

    document.execCommand

--- minerva/src/main/resources/web/js/copy.js
+++ minerva/src/main/resources/web/js/copy.js
@@ -1,9 +1,12 @@
 function copy(copyBtn, text) {
     let temp = $('<input>')
     $('body').append(temp)
-    temp.val(text).select()
-    Document.execCommand('copy')
-    temp.remove()
+    try {
+        temp.val(text).select()
+        document.execCommand('copy')
+    } finally {
+        temp.remove()
+    }
     copyBtn.style.color = '#0a0'
     copyBtn.style.transform = 'scale(1.2) rotate(10deg)'
     setTimeout(() => { copyBtn.style.color = ''; copyBtn.style.transform = ''; }, 500)
