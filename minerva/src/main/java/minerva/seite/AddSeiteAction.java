34e80b64be408da41a32aeeaab3022d54c009cb6
Marcus Warm <marcus.warm@x-map.de>
2025-01-29 15:04:05 +0100

    Add Below ft_ tag Not Allowed

--- minerva/src/main/java/minerva/seite/AddSeiteAction.java
+++ minerva/src/main/java/minerva/seite/AddSeiteAction.java
@@ -1,15 +1,23 @@
 package minerva.seite;
 
 import minerva.book.BAction;
+import minerva.model.BookSO;
 
 public class AddSeiteAction extends BAction {
+	public static AddSeiteAllowed addSeiteAllowed = (book, parent) -> {};
 
     @Override
     protected void execute() {
         String parentId = ctx.pathParam("parentid");
 
+        addSeiteAllowed.checkIfAllowed(book, parentId);
         String id = user.createSeite(branch, bookFolder, parentId);
 
         ctx.redirect("/s-edit/" + esc(branch) + "/" + esc(bookFolder) + "/" + esc(id));
+    }
+    
+    public interface AddSeiteAllowed {
+    	
+    	void checkIfAllowed(BookSO book, String parentId);
     }
 }
