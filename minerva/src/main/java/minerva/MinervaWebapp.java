df26bce39b189343062c0fa7244ab6141cd099cf
Marcus Warm <marcus.warm@x-map.de>
2025-03-21 08:18:16 +0100

    GTC

--- minerva/src/main/java/minerva/MinervaWebapp.java
+++ minerva/src/main/java/minerva/MinervaWebapp.java
@@ -159,7 +159,7 @@ import minerva.workspace.WorkspacesPage;
 import spark.Spark;
 
 public class MinervaWebapp extends RouteDefinitions {
-    public static final String VERSION = "3.10.0";
+    public static final String VERSION = "3.11.0";
     private static MinervaFactory factory;
     
     @Override
