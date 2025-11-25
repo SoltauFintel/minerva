89a32fe59b4f970422244328f919ccd648930b8c
Marcus Warm <marcus.warm@x-map.de>
2025-11-25 15:58:42 +0100

    Manual testcases

---
+++ minerva/src/main/java/minerva/base/Sequence.java
@@ -0,0 +1,32 @@
+package minerva.base;
+
+import java.io.File;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import github.soltaufintel.amalia.base.FileService;
+import minerva.MinervaWebapp;
+
+public class Sequence {
+	private static final ConcurrentMap<String, Object> locks = new ConcurrentHashMap<>();
+	private final String key;
+	private final Object keylock;
+
+	public Sequence(String key) {
+		this.key = key;
+		keylock = locks.computeIfAbsent(key, k -> new Object());
+	}
+
+	public int nextNumber() {
+		synchronized (keylock) {
+			File file = new File(MinervaWebapp.factory().getConfig().getWorkspacesFolder() + "/" + key + ".sequence");
+			String content = FileService.loadPlainTextFile(file);
+			if (content == null) {
+				content = "0";
+			}
+			int value = Integer.parseInt(content) + 1;
+			FileService.savePlainTextFile(file, "" + value);
+			return value;
+		}
+	}
+}
